e0c702395100f6d1bf4a133b82725a32013ce2b1
guigui64 <guillaume.comte10@gmail.com>
2019-04-22 17:57:22 +0200

    messages (service in service)

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,14 +3,17 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO add message AFTER getting the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
